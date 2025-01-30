01fbc6e29c2735bace83e3e004e7e35105d5bb10
Shahil <shahil.b2023cse@sece.ac.in>
2025-01-30 21:59:55 +0530

    Vite config fixed

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,8 @@ export default defineConfig({
     proxy: {
       '/api': {
           target: 'https://netflix-back-1.onrender.com',
+          changeOrigin: true,
+        secure: false,
           
       },
   }
