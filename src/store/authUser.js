12cb9e67de97af681437e730b84e80eb34f553db
Shahil <shahil.b2023cse@sece.ac.in>
2025-01-31 09:36:58 +0530

    added env varibles

--- src/store/authUser.js
+++ src/store/authUser.js
@@ -13,7 +13,7 @@ export const useAuthStore = create((set) => ({
 	signup: async (credentials) => {
 		set({ isSigningUp: true });
 		try {
-			const response = await axios.post(`${import.meta.env.VITE_BASE}/api/v1/auth/signup`, credentials);
+			const response = await axios.post(`https://netflix-back-1.onrender.com/api/v1/auth/signup`, credentials);
 			set({ user: response.data.user, isSigningUp: false });
 			toast.success("Account created successfully");
 		} catch (error) {
@@ -24,7 +24,8 @@ export const useAuthStore = create((set) => ({
 	login: async (credentials) => {
 		set({ isLoggingIn: true });
 		try {
-			const response = await axios.post(`${import.meta.env.VITE_BASE}/api/v1/auth/login`, credentials);
+			const response = await axios.post(`https://netflix-back-1.onrender.com
+/api/v1/auth/login`, credentials);
 			set({ user: response.data.user, isLoggingIn: false });
 		} catch (error) {
 			set({ isLoggingIn: false, user: null });
@@ -34,7 +35,7 @@ export const useAuthStore = create((set) => ({
 	logout: async () => {
 		set({ isLoggingOut: true });
 		try {
-			await axios.post("/api/v1/auth/logout");
+			await axios.post("https://netflix-back-1.onrender.com/api/v1/auth/logout");
 			set({ user: null, isLoggingOut: false });
 			toast.success("Logged out successfully");
 		} catch (error) {
@@ -45,7 +46,7 @@ export const useAuthStore = create((set) => ({
 	authCheck: async () => {
 		set({ isCheckingAuth: true });
 		try {
-			const response = await axios.get("/api/v1/auth/authCheck");
+			const response = await axios.get("https://netflix-back-1.onrender.com/api/v1/auth/authCheck");
 
 			set({ user: response.data.user, isCheckingAuth: false });
 		} catch (error) {
